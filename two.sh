c5f187f94ee364c08f2fec9e64f06c0a067e77fb
rewanthsaai <rewanthsaai@gmail.com>
2022-08-08 15:54:58 +0530

    if else

---
+++ two.sh
@@ -0,0 +1,22 @@
+
+        
+
+
+           class 
+
+	   { 
+		   public static void main[string[]args] 
+
+		   if
+			   { 
+				   prime number are not 
+			   } 
+		   else 
+			   {
+				   prime number 
+	                    } 
+			    system.out.println("prime number") 
+		    } 
+	    }
+
+
